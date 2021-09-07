41a6d3c4fe4c3291173c4b248076810ced84961a
rahinic <rahini.c@gmail.com>
2021-09-08 00:05:19 +0200

    Dataset fetch fix

--- notebooks/fetch_dataset.py
+++ notebooks/fetch_dataset.py
@@ -2,101 +2,160 @@
 
 import json
 import pickle
+from typing import Type
 import torch
-# from torch._C import int64
+from torch.utils.data import Dataset, DataLoader
 
-def fetch_jsonfile(input_dataset_file):
-    """returns the .json format of the requested dataset file"""
-    filepath = "data/interim/"
-    with open(filepath+input_dataset_file,'r') as f:
-        contents = json.load(f)
-
-    return contents
-
-def load_lkp_tables():
-    """returns W, T and P look-up tables"""
-    print("loading dictionaries....")
-    filepath = "data/interim/"
-    lkp_tbl_lst = ['tokens_lkp.pkl','tags_lkp.pkl','targets_lkp.pkl']
-
-    def pkl_load(input):
-        f = open(input,'rb')
-        return pickle.load(f)
+class pennDataset(Dataset):
     
-    tokens_lkp = pkl_load(filepath+lkp_tbl_lst[0])
-    tags_lkp = pkl_load(filepath+lkp_tbl_lst[1])
-    targets_lkp = pkl_load(filepath+lkp_tbl_lst[2])
+    def fetch_jsonfile(self, input_dataset_file):
+        """returns the .json format of the requested dataset file"""
+        filepath = "data/interim/"
+        with open(filepath+input_dataset_file,'r') as f:
+            contents = json.load(f)
 
-    print("done")
+        return contents
 
-    return tokens_lkp, tags_lkp, targets_lkp
+    def load_lkp_tables(self):
+        """returns W, T and P look-up tables"""
+        print("loading dictionaries....")
+        filepath = "data/interim/"
+        lkp_tbl_lst = ['tokens_lkp.pkl','tags_lkp.pkl','targets_lkp.pkl']
 
-
-def file_processor(input_dataset):
-
-    """
-    Does the following:
-        1. Fetch the JSON file of the requested dataset file
-        2. Fetch the 3 look-up tables W, T and P
-        3. Defines the tokens/tags/targets to idx conversion pipelines
-        4. depending on the type of list(subllist, no sublist, mix of both), uses #3 and makes the conversion to idx
-        5. idx to tensor conversion 
-    """
-
-    dataset_samples = fetch_jsonfile(input_dataset) # Step 1
-    token_lkp_tbl, tags_lkp_tbl, tgt_lkp_tbl = load_lkp_tables() # Step 2
-
-
-    # Step 3
-    def sample_pipeline(x):
+        def pkl_load(input):
+            f = open(input,'rb')
+            return pickle.load(f)
         
-        curr_token_to_idx_list = []
-        sub_lst = []
-        for lst in x:
-            if len(lst) == 1:
-                try:
-                    # print(f"The token {lst[0]} has the idx: {token_lkp_tbl[lst[0]]}")
-                    sub_lst.append([token_lkp_tbl[lst[0]]])
-                    # print("SU")
-                except TypeError:
-                    print(f"Type Error here: {lst[0]}")
+        tokens_lkp = pkl_load(filepath+lkp_tbl_lst[0])
+        tags_lkp = pkl_load(filepath+lkp_tbl_lst[1])
+        targets_lkp = pkl_load(filepath+lkp_tbl_lst[2])
+
+        print("done")
+
+        return tokens_lkp, tags_lkp, targets_lkp
+
+
+    def file_processor(self,input_dataset):
+
+        """
+        Does the following:
+            1. Fetch the JSON file of the requested dataset file
+            2. Fetch the 3 look-up tables W, T and P
+            3. Defines the tokens/tags/targets to idx conversion pipelines
+            4. depending on the type of list(subllist, no sublist, mix of both), uses #3 and makes the conversion to idx
+            5. idx to tensor conversion 
+        """
+
+        dataset_samples = self.fetch_jsonfile(input_dataset) # Step 1
+        token_lkp_tbl, tags_lkp_tbl, tgt_lkp_tbl = self.load_lkp_tables() # Step 2
+
+
+        # Step 3
+        def sample_to_idx_pipeline(input_sample_x, input_lkp_tbl):
+            curr_x_to_idx_list, sub_lst, sub_lst_new = [], [], []
+
+            for lst in input_sample_x:
                 
-            else:
-                print(f"Big sublist:{lst}")
-                sub_lst.append([token_lkp_tbl[tok] for tok in lst])
+                if len(lst)==1:
+                    try:
+                        sub_lst.append([input_lkp_tbl[lst[0]]])
+                    except TypeError:
+                        print(f"Type Error here: {lst[0]}")
+                else:
+                    try:
+                        sub_lst.append([input_lkp_tbl[item] for item in lst])                    
+                    except KeyError:
+                        sub_lst.append(input_lkp_tbl[lst])
+            curr_x_to_idx_list.append(sub_lst)
 
-        curr_token_to_idx_list.append(torch.tensor(sub_lst))
-            # print(sub_lst)
+            return curr_x_to_idx_list[0]
+        dataset = []
+        for sample in dataset_samples:
+            max_level = list(sample.keys())[-1]
+            # (ii) Create a skeletol dict similar to this example dict of JSON obj
+
+            dict_as_numbers = {
+                str(level): {"tokens": list(), "tags": list(), "targets": list()}
+                for level in range(2, int(max_level)+1)
+            }
+
+            for level in sample.items():
+
+                curr_token_list = level[1]['tokens']
+                curr_token_list_as_numbers = sample_to_idx_pipeline(curr_token_list,input_lkp_tbl=token_lkp_tbl)
+                dict_as_numbers[level[0]]['tokens'] = curr_token_list_as_numbers
+                # print(dict_as_numbers)
+
+                curr_tag_list = level[1]['tags']        
+                # print(curr_tag_list)
+                curr_tags_list_as_numbers = sample_to_idx_pipeline(curr_tag_list,input_lkp_tbl=tags_lkp_tbl)
+                # print(curr_tags_list_as_numbers)
+                dict_as_numbers[level[0]]['tags'] = curr_tags_list_as_numbers
+
+                curr_tgt_list = level[1]['targets']        
+                curr_tgt_list_as_numbers = sample_to_idx_pipeline(curr_tgt_list,input_lkp_tbl=tgt_lkp_tbl)
+                dict_as_numbers[level[0]]['targets'] = curr_tgt_list_as_numbers 
+
+                dataset.append(dict_as_numbers)
+            
+            return dataset
+#--------------------------------------------------------------
+    def __init__(self, currDataset=None):
+
+        self.mydataset = currDataset
+        # self.vocabulary, self.pos_tags, self.target_tags = self.load_lkp_tables()
+        self.all_samples = self.file_processor(input_dataset=self.mydataset)
+        # self.samples = self.file_parser(self.all_samples)
+
+    def __len__(self):
+        print(len(self.all_samples))
+
+        return len(self.all_samples)
+
+    def __getitem__(self, idx) :
+
+        return self.all_samples[idx]   
+
+valid_ds = DataLoader(dataset=pennDataset('valid_dataset_transformed.json'), batch_size=8, shuffle=False)
+
+for idx, ex in enumerate(valid_ds):
+    if idx>0:
+        break
+    print(len(ex))
+    print(ex)
+        # # (i) Consider one example object from our JSON file
+        # example = dataset_samples[8]
+        # max_level = list(example.keys())[-1]
+        # print(f"One example: {example}")
+        # # print(f"Last level here is : {max_level}")
+        
+        # # (ii) Create a skeletol dict similar to this example dict of JSON obj
+
+        # dict_as_numbers = {
+        #     str(level): {"tokens": list(), "tags": list(), "targets": list()}
+        #     for level in range(2, int(max_level)+1)
+        # }
+
+        # # (iii) 
+        # for level in example.items():
+
+        #     curr_token_list = level[1]['tokens']
+        #     curr_token_list_as_numbers = sample_to_idx_pipeline(curr_token_list,input_lkp_tbl=token_lkp_tbl)
+        #     dict_as_numbers[level[0]]['tokens'] = curr_token_list_as_numbers
+        #     # print(dict_as_numbers)
+
+        #     curr_tag_list = level[1]['tags']        
+        #     # print(curr_tag_list)
+        #     curr_tags_list_as_numbers = sample_to_idx_pipeline(curr_tag_list,input_lkp_tbl=tags_lkp_tbl)
+        #     print(curr_tags_list_as_numbers)
+        #     dict_as_numbers[level[0]]['tags'] = curr_tags_list_as_numbers
+
+        #     curr_tgt_list = level[1]['targets']        
+        #     curr_tgt_list_as_numbers = sample_to_idx_pipeline(curr_tgt_list,input_lkp_tbl=tgt_lkp_tbl)
+        #     dict_as_numbers[level[0]]['targets'] = curr_tgt_list_as_numbers      
 
 
-        return curr_token_to_idx_list
+        #     print('------') 
+        # print(dict_as_numbers)
 
-    def tags_pipeline(x):
-        return [tags_lkp_tbl[tag] for tag in x]
-
-    def tgt_pipeline(x):
-        return [tgt_lkp_tbl[tgt] for tgt in x]
-    
-    # (i) Consider one example object from our JSON file
-    example = dataset_samples[8]
-    max_level = list(example.keys())[-1]
-    print(f"One example: {example}")
-    print(f"Last level here is : {max_level}")
-    
-    # (ii) Create a skeletol dict similar to this example dict of JSON obj
-    
-    dict_as_numbers = {
-        level: {"tokens": list(), "tags": list(), "targets": list()}
-        for level in range(2, int(max_level)+1)
-    }
-
-    # (iii) 
-    for level in example.items():
-        print(level[1]['tokens'])
-        curr_token_list = level[1]['tokens']
-        print(sample_pipeline(curr_token_list))
-
-        print('------') 
-    # print(dict_as_numbers)
-
-file_processor('test_dataset_transformed.json')
+# file_processor('test_dataset_transformed.json')
